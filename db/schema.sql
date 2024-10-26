0577ecad415754ae250974198123e9dae7e038cf
Matthew Davis <mdavi359@yahoo.com>
2024-10-26 16:59:32 -0400

    Debugged seeds.sql\

--- db/schema.sql
+++ db/schema.sql
@@ -4,15 +4,15 @@ CREATE DATABASE employees_db;
 \c employees_db;
 
 CREATE TABLE departments (
-  department_id SERIAL PRIMARY KEY NOT NULL,
+  department_id SERIAL PRIMARY KEY,
   department_name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE roles (
-    role_id SERIAL PRIMARY KEY NOT NULL,
+    role_id SERIAL PRIMARY KEY,
     role_name VARCHAR(60) NOT NULL,
     salary INT NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id)
     REFERENCES departments(department_id)
     ON DELETE SET NULL
@@ -23,7 +23,7 @@ CREATE TABLE employees (
     first_name VARCHAR(60) NOT NULL,
     last_name VARCHAR(60) NOT NULL,
     manager VARCHAR(100) NOT NULL,
-    role_id INT NOT NULL
+    role_id INT NOT NULL,
     FOREIGN KEY (role_id)
     REFERENCES roles(role_id)
     ON DELETE SET NULL
