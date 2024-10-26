b8f810c0740ca59d408af2742c32168410190fc1
Matthew Davis <mdavi359@yahoo.com>
2024-10-26 14:54:14 -0400

    Updated the table information in the schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,32 @@
-DROP DATABASE IF EXISTS movies_db;
-CREATE DATABASE movies_db;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-\c movies_db;
+\c employees_db;
 
-CREATE TABLE movies (
-  id SERIAL PRIMARY KEY,
-  movie_name VARCHAR(100) NOT NULL
+CREATE TABLE departments (
+  department_id SERIAL PRIMARY KEY NOT NULL,
+  department_name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE reviews (
+CREATE TABLE roles (
+    role_id SERIAL PRIMARY KEY NOT NULL,
+    role_name VARCHAR(60) NOT NULL,
+    salary INT NOT NULL,
+    department_name VARCHAR(100) NOT NULL,
+    department_id INT NOT NULL
+    FOREIGN KEY (department_id)
+    REFERENCES departments(department_id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
-    movie_id INT,
-    review TEXT NOT NULL,
-    FOREIGN KEY (movie_id)
-    REFERENCES movies(id)
+    first_name VARCHAR(60) NOT NULL,
+    last_name VARCHAR(60) NOT NULL,
+    role_name VARCHAR(60) NOT NULL,
+    manager VARCHAR(100) NOT NULL,
+    role_id INT NOT NULL
+    FOREIGN KEY (role_id)
+    REFERENCES roles(role_id)
     ON DELETE SET NULL
 );
